f11ee80ce66f5156ad9824ad230bb676a5a3236b
stevenprimeaux <stevenprimeaux@gmail.com>
2023-06-30 17:26:55 -0500

    Test auth functions. Add teardown.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,11 @@ from sqlalchemy import text
 
 from flaskr import create_app, db, get_database_url, get_secret_key
 
+
 with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
     _data_sql = f.read().decode("utf8")
+with open(os.path.join(os.path.dirname(__file__), "teardown.sql"), "rb") as f:
+    _teardown_sql = f.read().decode("utf8")
 
 
 @pytest.fixture
@@ -19,10 +22,34 @@ def app():
 
     with app.app_context():
         db.session.execute(text(_data_sql))
+        db.session.commit()
 
     yield app
+
+    with app.app_context():
+        db.session.execute(text(_teardown_sql))
+        db.session.commit()
 
 
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+class AuthActions(object):
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username="test", password="test"):
+        return self._client.post(
+            "/auth/login",
+            data={"username": username, "password": password}
+        )
+
+    def logout(self):
+        return self._client.get("/auth/logout")
+
+
+@pytest.fixture
+def auth(client):
+    return AuthActions(client)
